3b2d070902e327fc1aef06ebae20cd988f2f22c9
arps <arpanp99@gmail.com>
2023-06-21 23:09:40 -0400

    Javadoc in Interface

--- src/RemoteInterface.java
+++ src/RemoteInterface.java
@@ -1,7 +1,20 @@
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
+/**
+ * The RemoteInterface interface defines methods that can be invoked remotely in a distributed Java application.
+ * It extends the Remote interface from the Java RMI (Remote Method Invocation) package.
+ */
 public interface RemoteInterface extends Remote {
 
-  String request(String operation,String key, String value) throws RemoteException;
+  /**
+   * Executes a remote request operation.
+   *
+   * @param operation the operation to be performed remotely
+   * @param key the key associated with the request
+   * @param value the value associated with the request
+   * @return a response message from the remote operation
+   * @throws RemoteException if a remote communication error occurs
+   */
+  String request(String operation, String key, String value) throws RemoteException;
 }
